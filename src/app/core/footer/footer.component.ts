9cdd6a98c3cc6661c844839a8d3ca739652192cf
danielbueno76 <danielbueno76@gmail.com>
2021-10-31 12:12:04 +0100

    define components and style

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'kc-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  autor: string;
+  empresa: string;
+  constructor() {
+    this.autor = '';
+    this.empresa = '';
   }
 
+  ngOnInit(): void {
+    this.autor = 'Daniel Bueno';
+    this.empresa = 'KeepCoding';
+  }
 }
